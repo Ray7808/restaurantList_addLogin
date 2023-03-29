2534d4b21dd209335c9f8cfd0ad95ad454424298
Ray7808 <yuwang7808@gmail.com>
2023-03-29 16:17:38 +0000

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -26,5 +26,55 @@ router.get("/search", (req, res) => {
             })
         )
 })
+router.get("/sort/:sortingMethod", (req, res) => {
+    //藉由輸入的query string，排序對應的餐廳資訊(名字、類別跟地區)
+    const sortingMethod = req.params.sortingMethod
+    switch (sortingMethod) {
+        case "AtoZ":
+            return restaurantInfo
+                .find()
+                .lean()
+                .sort({ name_en: "asc" })
+                .then((restaurants) =>
+                    res.render("index", {
+                        restaurants: restaurants,
+                    })
+                )
+                .catch((error) => console.log(error))
+        case "ZtoA":
+            return restaurantInfo
+                .find()
+                .lean()
+                .sort({ name_en: "desc" })
+                .then((restaurants) =>
+                    res.render("index", {
+                        restaurants: restaurants,
+                    })
+                )
+                .catch((error) => console.log(error))
+        case "category":
+            return restaurantInfo
+                .find()
+                .lean()
+                .sort({ category: "asc" })
+                .then((restaurants) =>
+                    res.render("index", {
+                        restaurants: restaurants,
+                    })
+                )
+                .catch((error) => console.log(error))
+        case "location":
+            return restaurantInfo
+                .find()
+                .lean()
+                .sort({ location: "asc" })
+                .then((restaurants) =>
+                    res.render("index", {
+                        restaurants: restaurants,
+                    })
+                )
+                .catch((error) => console.log(error))
+    }
+})
 
 module.exports = router
