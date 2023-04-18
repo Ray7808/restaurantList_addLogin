b10eb5c356a8958c3eeaf2d8955e0722ac28370c
Ray7808 <yuwang7808@gmail.com>
2023-04-18 22:12:24 +0800

    fead: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,5 +5,10 @@ const router = express.Router()
 router.get('/login', (req, res) => {
     res.render('login')
 })
+router.post('/login', (req, res) => {})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
 
 module.exports = router
